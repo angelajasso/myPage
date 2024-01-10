fb28c95172671d7fdcf7a01a23a388100733ad28
angelajasso <angelajasso@hotmail.com>
2024-01-10 01:26:09 -0600

    adding languages

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 const slider = document.getElementById('slider');
 
+const flagsElement = document.getElementById("flags");
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) => {
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+
+    for (const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        textToChange.innerHTML = texts[section][value];
+    }
+};
+
+flagsElement.addEventListener('click', (e)=> {
+    changeLanguage(e.target.parentElement.dataset.language);
+});
+
 const setTheme = (theme) => {
     document.documentElement.setAttribute('data-theme', theme);
     localStorage.setItem('theme', theme);
