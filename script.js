a676ba4de0c53441257f2e35711b1dc4ec69b17d
angelajasso <angelajasso@hotmail.com>
2024-01-11 23:04:43 -0600

    code details

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-const slider = document.getElementById('slider');
+const preferedColorScheme = window.matchMedia("(prefers-color-scheme: dark)")
+  .matches
+  ? "dark"
+  : "light";
+const slider = document.getElementById("slider");
 
 const flagsElement = document.getElementById("flags");
 const textsToChange = document.querySelectorAll("[data-section]");
 
 const changeLanguage = async (language) => {
-    const requestJson = await fetch(`./languages/${language}.json`);
-    const texts = await requestJson.json();
+  const requestJson = await fetch(`./languages/${language}.json`);
+  const texts = await requestJson.json();
 
-    for (const textToChange of textsToChange) {
-        const section = textToChange.dataset.section;
-        const value = textToChange.dataset.value;
+  for (const textToChange of textsToChange) {
+    const section = textToChange.dataset.section;
+    const value = textToChange.dataset.value;
 
-        textToChange.innerHTML = texts[section][value];
-    }
+    textToChange.innerHTML = texts[section][value];
+  }
 };
 
-flagsElement.addEventListener('click', (e)=> {
-    changeLanguage(e.target.parentElement.dataset.language);
+flagsElement.addEventListener("click", (e) => {
+  changeLanguage(e.target.parentElement.dataset.language);
 });
 
 const setTheme = (theme) => {
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme);
-}
+  document.documentElement.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
+};
 
-slider.addEventListener('click', ()  => {
-    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
-    setTheme(switchToTheme);
+slider.addEventListener("click", () => {
+  let switchToTheme =
+    localStorage.getItem("theme") === "dark" ? "light" : "dark";
+  setTheme(switchToTheme);
 });
 
-setTheme(localStorage.getItem('theme') || preferedColorScheme);
+setTheme(localStorage.getItem("theme") || preferedColorScheme);
